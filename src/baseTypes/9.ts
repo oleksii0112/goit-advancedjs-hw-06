74e622e1372aa13a64f7519fa3cd0bfde1389652
Oleksii0112 <paganizondatr75@gmail.com>
2026-06-26 14:40:13 +0300

    rev1

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,6 +1,16 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type data = {
+  title: string;
+  likes: number,
+  accounts: string[],
+  status: string,
+  details?: {
+    createAt: Date,
+    updateAt: Date
+  }
+}
 
 const page1 = {
   title: 'The awesome page',
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface a {
+  title: string
+}
+
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<a> {
   pageInfo () {
     console.log(this.props.title);
   }
